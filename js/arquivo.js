19471bd43f146ab2e3e291e980279b7644f6842f
murilo218 <muriloalexandre978@gmail.com>
2025-07-01 12:25:25 -0300

    aula de JavaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,24 @@
+var global = "variavel global";
+console.log(global);
+
+function exemplo2() {
+    var local = "varivael local";
+    console.log(local);
+}
+
+exemplo2();
+
+let x = 10;
+if (x > 5) {
+    let y = 20;
+    console.log("Valor de y: ", y)
+}
+
+console.log(x);
+
+const _constante = "Texto";
+console.log(_constante);
+
+function saudacao() {
+    alert("Bem vindo ao nosso site3!");
+}
